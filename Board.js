0abd212c5bc06f57be51e2b556cebc98eacc5fdb
bharathc <bharathc@gmail.com>
2016-11-29 22:58:36 -0500

    Fixed remaining issues in boat placement on grid

--- Board.js
+++ Board.js
@@ -23,34 +23,43 @@ Board.prototype.assignRandBoatLocations=function(grid){
 		var row = Math.floor((Math.random()*10)+0);
 		var column = Math.floor((Math.random()*10)+0);
 		var direction = Math.round(Math.random());			//gen 0(horizantal) or 1(vertical)
-		// console.log(row+'	'+column);
-		// console.log(direction);
-		// console.log(grid[row][column]);
 	
 		if(direction==0 && column+4<=10){
-			var limit = column+4;
-			for(column;column<limit;column++){
-				if(grid[row][column]=='0'){
-					grid[row][column] = 'X';
+			//change if statement to be more efficent
+			if(grid[row][column]=='0'&&grid[row][column+3]=='0'&&grid[row][column+2]=='0'&&grid[row][column+1]=='0'){
+				var limit = column+4;
+				for(column;column<limit;column++){
+					grid[row][column]='X';
 				}
-				else{continue;}
+				count++;
 			}
-			count++;
 		}
 
 		else if(direction==1 && row+4<=10){
-			var limit = row+4;
-			for(row;row<limit;row++){
-				if(grid[row][column]=='0'){
-					grid[row][column] = 'X';
+			//change if statement to be more efficent
+			if(grid[row][column]=='0'&&grid[row+3][column]=='0'&&grid[row+2][column]=='0'&&grid[row+1][column]=='0'){
+				var limit = row+4;
+				for(row;row<limit;row++){
+					grid[row][column]='X';
 				}
-				else{continue;}
+				count++;
 			}
-			count++;
 		}
 	}
 };
 
+Board.prototype.printBoard= function(grid){
+	for(var i =0;i<10;i++){
+		var line = ""
+		for(var j=0;j<10;j++){
+			line = line+''+grid[i][j]+' ';
+		}
+		console.log(line);
+	}
+}
+
 var a = new Board();
 a.assignRandBoatLocations(a._grid);
-console.log(a._grid);
+console.log('\n');
+a.printBoard(a._grid);
+console.log('\n');
